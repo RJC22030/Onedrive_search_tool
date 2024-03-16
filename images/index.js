60e1a341891642726b52bd7a4ab41f1104f81461
RJC22030 <160463749+RJC22030@users.noreply.github.com>
2024-03-16 20:50:55 +0530

    Update index.js

--- images/index.js
+++ images/index.js
@@ -35,7 +35,7 @@ const msalconfig = {
     clientId: "5047e9a3-2b87-4fc9-abd8-9e85cca0e02d",
     authortity: "https://login.microsoftonline.com/common/",
     //Replace the localhost url below with ur permanent webpage url
-    redirectUri: "http://localhost:5502/",
+    redirectUri: "https://sairajobs.onrender.com",
   },
   cache: {
     cacheLocation: "sessionstorage",
