f889ec6935dae5fbf65e527792021494e5a8b56a
RJC22030 <160463749+RJC22030@users.noreply.github.com>
2024-06-02 12:02:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,51 +10,6 @@ const port = process.env.PORT || 5502; // Use PORT environment variable or defau
 app.use(morgan("dev"));
 app.use(express.json()); // Middleware to parse JSON request bodies
 
-async function uploadFileToDrive(filePath, client, folderPath) {
-  try {
-    const fileContent = fs.readFileSync(filePath);
-    await client
-      .api(`/me/drive/root:/${folderPath}/${path.basename(filePath)}:/content`)
-      .put(fileContent);
-    console.log("File uploaded successfully");
-  } catch (error) {
-    console.error("Error uploading file:", error);
-  }
-}
-
-// API endpoint to trigger OneDrive file upload
-app.post("/upload-to-onedrive", (req, res) => {
-  const { filePath, accessToken, destPath } = req.body;
-  if (!filePath || !accessToken || !destPath) {
-    return res
-      .status(400)
-      .send("File path, access token, and folder path are required");
-  }
-
-  const client = Client.init({
-    authProvider: (done) => {
-      done(null, accessToken);
-    },
-  });
-
-  // Upload all existing files in the folder
-  const files = fs.readdirSync(filePath);
-  files.forEach((filename) => {
-    const fullPath = path.join(filePath, filename);
-    uploadFileToDrive(fullPath, client, destPath);
-  });
-
-  // Watch for changes in the folder
-  fs.watch(filePath, (eventType, filename) => {
-    if (filename && eventType === "change") {
-      console.log(`File ${filename} has been modified`);
-      uploadFileToDrive(path.join(filePath, filename), client, destPath);
-    }
-  });
-
-  res.send("Folder monitoring started successfully");
-});
-
 // Serve static files
 app.use(express.static("public"));
 app.use("/images", express.static(path.join(__dirname, "images")));
@@ -66,4 +21,3 @@ app.get("/", function (req, res) {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-
