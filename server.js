eace017792aea7a47618adcc5f7de3654a80aef1
RJC22030 <160463749+RJC22030@users.noreply.github.com>
2024-06-02 10:39:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,63 +1,27 @@
 const express = require("express");
-const cors = require("cors"); // Import CORS middleware
+const cors = require("cors");
 const morgan = require("morgan");
 const path = require("path");
-const fs = require("fs");
-const { Client } = require("@microsoft/microsoft-graph-client");
+const uploadToOneDrive = require("./routes/upload-to-onedrive");
 
 const app = express();
 const port = process.env.PORT || 5502;
-app.use(cors()); // Enable CORS
+
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
-async function uploadFileToDrive(filePath, client, folderPath) {
-  try {
-    const fileContent = fs.readFileSync(filePath);
-    await client
-      .api(`/me/drive/root:/${folderPath}/${path.basename(filePath)}:/content`)
-      .put(fileContent);
-    console.log("File uploaded successfully");
-  } catch (error) {
-    console.error("Error uploading file:", error);
-  }
-}
-
-app.post("/upload-to-onedrive", (req, res) => {
-  const { filePath, accessToken, destPath } = req.body;
-  if (!filePath || !accessToken || !destPath) {
-    return res
-      .status(400)
-      .send("File path, access token, and folder path are required");
-  }
-
-  const client = Client.init({
-    authProvider: (done) => {
-      done(null, accessToken);
-    },
-  });
-
-  const files = fs.readdirSync(filePath);
-  files.forEach((filename) => {
-    const fullPath = path.join(filePath, filename);
-    uploadFileToDrive(fullPath, client, destPath);
-  });
-
-  fs.watch(filePath, (eventType, filename) => {
-    if (filename && eventType === "change") {
-      console.log(`File ${filename} has been modified`);
-      uploadFileToDrive(path.join(filePath, filename), client, destPath);
-    }
-  });
-
-  res.send("Folder monitoring started successfully");
-});
-
+// Serve static files from the public directory
 app.use(express.static("public"));
+
+// Serve images from the images directory
 app.use("/images", express.static(path.join(__dirname, "images")));
 
+// Use the upload-to-onedrive route
+app.use("/upload-to-onedrive", uploadToOneDrive);
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(port, () => {
